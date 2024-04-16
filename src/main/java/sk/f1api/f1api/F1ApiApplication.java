e9de9f02e8bda8cab347a3deb16d9dfd91e2c864
Michal Knor <michal.knor@tecs.at>
2024-04-16 14:41:12 +0200

    first commit

---
+++ src/main/java/sk/f1api/f1api/F1ApiApplication.java
@@ -0,0 +1,13 @@
+package sk.f1api.f1api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/f1api/f1api/F1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.f1api.f1api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
