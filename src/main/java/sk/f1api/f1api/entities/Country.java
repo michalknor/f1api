5d934b62679bae3af3e6ff6d9b8bfcca33d47c8a
Kvakino <mikeknor2503@gmail.com>
2024-05-01 23:16:58 +0200

    create entity country

---
+++ src/main/java/sk/f1api/f1api/entities/Country.java
@@ -0,0 +1,18 @@
+package sk.f1api.f1api.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Country {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "country_abbreviation", nullable = false, unique = true)
+	private String countryAbbreviation;
+}
