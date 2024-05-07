5fe581b3d9d09eecfed4fe28bd9d95405c8b4607
Kvakino <mikeknor2503@gmail.com>
2024-05-08 00:55:29 +0200

    add Identifiable interface to Country and implements isDuplicate

--- src/main/java/sk/f1api/f1api/entity/Country.java
+++ src/main/java/sk/f1api/f1api/entity/Country.java
@@ -1,13 +1,20 @@
 package sk.f1api.f1api.entity;
 
+import java.util.List;
+
+import org.hibernate.Session;
+
 import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-public class Country {
+public class Country implements Identifiable {
 
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +25,17 @@ public class Country {
 
     @Column(nullable = false, unique = true, length = 30)
     private String name;
+
+    @Override
+    public boolean isDuplicate(Session session) {
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Country> criteria = cb.createQuery(Country.class);
+        Root<Country> root = criteria.from(Country.class);
+
+        criteria.select(root).where(cb.equal(root.get("abbreviation"), abbreviation));
+
+        List<Country> countries = session.createQuery(criteria).getResultList();
+
+        return countries.size() == 1;
+    }
 }
