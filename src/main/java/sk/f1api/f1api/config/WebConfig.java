5fb46aa0e2a317ecb30980a753da2fbef9324ed3
Kvakino <mikeknor2503@gmail.com>
2024-07-09 22:06:13 +0200

    add path prefix /api for all classes in controller package

---
+++ src/main/java/sk/f1api/f1api/config/WebConfig.java
@@ -0,0 +1,14 @@
+package sk.f1api.f1api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(@SuppressWarnings("null") PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api", cls -> cls.getPackage().getName().equals("sk.f1api.f1api.controller"));
+    }
+}
